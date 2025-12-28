48b56a805763f13e0b466a97c04092917a724703
hzz <383686357@qq.com>
2025-12-28 20:23:28 +0800

    update

--- viewer/src/components/DicomViewport/index.jsx
+++ viewer/src/components/DicomViewport/index.jsx
@@ -11,10 +11,31 @@ export default function DicomViewport({ imageId }) {
         cornerstone.displayImage(element, image);
       });
 
-      const WwwcTool = cornerstoneTools.WwwcTool;
+      // const WwwcTool = cornerstoneTools.WwwcTool;
+      // cornerstoneTools.addTool(WwwcTool);
+      // cornerstoneTools.setToolActive("Wwwc", { mouseButtonMask: 1 });
 
-      cornerstoneTools.addTool(WwwcTool);
-      cornerstoneTools.setToolActive("Wwwc", { mouseButtonMask: 1 });
+      // const PanTool = cornerstoneTools.PanTool;
+      // cornerstoneTools.addTool(PanTool);
+      // cornerstoneTools.setToolActive("Pan", { mouseButtonMask: 1 });
+
+      // const RotateTool = cornerstoneTools.RotateTool;
+      // cornerstoneTools.addTool(RotateTool);
+      // cornerstoneTools.setToolActive("Rotate", { mouseButtonMask: 1 });
+
+      // const CobbAngleTool = cornerstoneTools.CobbAngleTool;
+      // cornerstoneTools.addTool(CobbAngleTool);
+      // cornerstoneTools.setToolActive("CobbAngle", { mouseButtonMask: 1 });
+
+      const TextMarkerTool = cornerstoneTools.TextMarkerTool;
+      const configuration = {
+        markers: ["F5", "F4", "F3", "F2", "F1"],
+        current: "F5",
+        ascending: true,
+        loop: true,
+      };
+      cornerstoneTools.addTool(TextMarkerTool, { configuration });
+      cornerstoneTools.setToolActive("TextMarker", { mouseButtonMask: 1 });
     }
   }, [imageId]);
 
